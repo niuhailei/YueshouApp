4aa62415229d45c3d1e19a6e7a3b4f95ae8d59a0
e29hz <ehongzhen@qq.com>
2016-06-20 09:16:03 +0800

    Initial Commit

---
+++ YueshouApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  YueshouApp
+//
+//  Created by 鄂鸿桢 on 16/6/20.
+//  Copyright © 2016年 xiaoke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ YueshouAppTests/YueshouAppTests.m
@@ -0,0 +1,39 @@
+//
+//  YueshouAppTests.m
+//  YueshouAppTests
+//
+//  Created by 鄂鸿桢 on 16/6/20.
+//  Copyright © 2016年 xiaoke. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface YueshouAppTests : XCTestCase
+
+@end
+
+@implementation YueshouAppTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
